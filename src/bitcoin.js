1248d50987fc38d92ba334c1eaa0bb37646763ac
Anton Cheng <anton@coolbitx.com>
2019-06-12 17:01:33 +0800

    BTC script decode test

--- src/bitcoin.js
+++ src/bitcoin.js
@@ -1,13 +1,28 @@
 const bitcoin = require('bitcoinjs-lib');
+const bscript = bitcoin.script
+const bscriptSig = bitcoin.script.signature
 
 module.exports = function (hex) {
-    const tx = bitcoin.Transaction.fromHex(hex);
+    let tx = bitcoin.Transaction.fromHex(hex)
+    
+    tx.ins.forEach(input=>{
+        input.hash = input.hash.toString('hex')
+        // input.sig = bscriptSig.decode(input.script)
+    })
+
+    tx.outs.forEach(output => {
+        output.script = bscript.toASM(output.script)
+    })
+
+    // Sum output values
     let totalValue = 0;
     if (tx && tx.outs && tx.outs.length > 0) {
-        totalValue = tx.outs.map(out => out.value).reduce((a, b) => {
-            a + b;
-        });
-        tx.totalValue = totalValue;    
+        totalValue = tx.outs.map(out => out.value).reduce(reducer)
+        tx.totalValue = totalValue
     }
+
     return tx;
-}
+};
+
+const reducer = (a, b) => a + b
+
