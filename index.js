54eb4d8bb92fe21ae66b8ab16c08104f2e5d28a9
Anton Cheng <anton@coolbitx.com>
2019-06-14 14:04:23 +0800

    Use function name src/btc

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     decodeEthRawTx: require("./src/eth.js"),
-    decodeBtcRawTx: require("./src/btc.js"),
+    decodeUTXORawTx: require("./src/btc.js"),
     decodeXrpRawTx: require('./src/xrp.js'),
     decodeBnbRawTx: require('./src/bnb')
 }
