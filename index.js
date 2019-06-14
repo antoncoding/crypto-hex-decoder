4d0904337bff06c0d406fd7c702f147797aec0b1
Anton Cheng <anton@coolbitx.com>
2019-06-14 14:11:56 +0800

    rename decodeRawUTXO

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 module.exports = {
     decodeEthRawTx: require("./src/eth.js"),
-    decodeUTXORawTx: require("./src/btc.js"),
+    decodeRawUTXO: require("./src/btc.js"),
     decodeXrpRawTx: require('./src/xrp.js'),
     decodeBnbRawTx: require('./src/bnb')
 }
