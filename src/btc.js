cd6bf5818a6a769f4d7c9fc3767e1dbfce996dbc
Anton Cheng <anton@coolbitx.com>
2019-06-14 11:54:20 +0800

    Add input script decode on p2pkh. return script as object

--- src/btc.js
+++ src/btc.js
@@ -5,17 +5,31 @@ module.exports = function (hex) {
     let tx = bitcoinjs.Transaction.fromHex(hex)
     
     tx.ins.forEach(input=>{
-        input.hash = input.hash.toString('hex')
-        input.script = input.script.toString('hex')
-        
         // Decode Witness Data
         if (input.witness.length > 0){
+            input.type = 'Segwit'
             let {signature, hashType} = bscript.signature.decode(input.witness[0])
             signature = signature.toString('hex')
             let publicKey = input.witness[1].toString('hex')
             input.witness = {signature, publicKey, hashType}
+            
+            input.script = {
+                hex: input.script.toString('hex')
+            }
+        } else {
+            let decodedScript = bscript.toASM(input.script).split(" ")
+            if(decodedScript.length === 2){
+                input.type = 'P2PKH'
+                input.script = {
+                    signature: decodedScript[0],
+                    publicKey: decodedScript[1]
+                }
+            }
+            else{
+                intput.script = {hex:decodedScript}
+            }
         }
-        
+        input.hash = input.hash.toString('hex')
     })
 
     tx.outs.forEach(output => {
