ebe6dc7409275ca9dd41bcc30359563a0bd26b23
Anton Cheng <anton@coolbitx.com>
2019-09-13 14:34:28 +0800

    Remove index in mainrepo

--- index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    decodeRawUTXO: require("./src/btc"),    
-    decodeBnbRawTx: require('./src/bnb'),
-    encodeBnbAddress: require('./src/utils/bnb_addr_util').encodeAddress,
-    decodeBnbAddress: require('./src/utils/bnb_addr_util').decodeAddress
-}
